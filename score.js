97e2340d16f0153398a7aadbc5825f6a05f8782b
Xavier Greig <eraxiv@gmail.com>
2015-12-07 20:49:51 +1100

    Update score.js

--- score.js
+++ score.js
@@ -1,11 +1,5 @@
-//var querystring = require('querystring');
-//var fs = require('fs');
-//var api_key = "P0X9953xJiY5ipcbjSaMTn5dARHjO4ty4FK7a+TnbEf8Zop9suRbY5oN8Vc6hgp63OEhRAGIigQdo6TbHrq22Q==";
-//var score_req = {};
-
-
 var http =  require('https');
-var debug =         0                   ;
+var debug = 0;
 
 
 module.exports =  {
@@ -28,7 +22,6 @@ module.exports =  {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        //'Accept': 'application/json',
         'Content-Length': payload.length,
         'Authorization': 'Bearer ' + apikey
       }
@@ -74,7 +67,7 @@ module.exports =  {
         return 0;
       }
 
-    }); //var post_req = http.request(post_options, function(res) {
+    }); 
 
     post_req.on('error', function(e) {
       console.log('error: ' + e.message);
@@ -83,10 +76,6 @@ module.exports =  {
 
     //post data
     if (payload) {
-        
-        //if(debug) console.log(payload); 
-        
-        
       post_req.write(payload);
       post_req.end();
     }
@@ -98,13 +87,8 @@ module.exports =  {
   ,start: function(apikey, workspace, services, job, payload, jobid, callback) {
 
     //stringify to get length
-    this.payload = '';//JSON.stringify(payload);
+    this.payload = '';
     var httpg = require('https');
-
-
-    console.log('start---------------------'); 
-    //if(debug) console.log(payload); 
-
 
     //setup options
     var post_options = {
@@ -113,9 +97,6 @@ module.exports =  {
       path: '/workspaces/' + workspace + '/services/' + services + '/' + job + '/' + jobid + '/start?api-version=2.0',
       method: 'POST',
       headers: {
-//        'Content-Type': 'application/json;charset=utf-8',
-//        'Accept': 'application/json',
-//        'Content-Length': 0,
         'Authorization': 'Bearer ' + apikey
       }
     };
@@ -133,7 +114,6 @@ module.exports =  {
         return null;
       }
 
-
       if (res.statusCode == 200) {
 
         var data = "";
@@ -146,42 +126,23 @@ module.exports =  {
         });
 
         res.on('end', function() {
-//          callback(JSON.parse(data));
-
-console.log(data); 
-
           callback(jobid);
         });
 
       } else {
         
-console.log('eeeeeeeeeend');         
-        
         return 0;
       }
 
-    }); //var post_req = http.request(post_options, function(res) {
+    }); 
 
     post_req.on('error', function(e) {
       console.log('error start: ' + e.message);
     });
 
-
-    //post data
-/*    if (this.payload) {
-      post_req.write(this.payload);
-      post_req.end();
-    }
-*/
-
-
-  } //start : function(score) {
-
-
-
-
+  } 
+  
   ,get: function(apikey, workspace, services, job, payload, jobid, callback) {
-
 
     var httpg = require('https');
 
@@ -192,30 +153,23 @@ console.log('eeeeeeeeeend');
       path: '/workspaces/' + workspace + '/services/' + services + '/' + job + '/' + jobid + '?api-version=2.0',
       method: 'GET',
       headers: {
-        //'Content-Type':'application/json;charset=utf-8',          
-        //'Accept': 'application/json',
         'Content-Length': 0,
         'Authorization': 'Bearer ' + apikey
       }
     };
 
-
     if(debug)
     console.log(get_options);  
 
-
     httpg.get(get_options, function(res) {
-
 
       if(debug)console.log('STATUS: ' + res.statusCode);
       if(debug)console.log('HEADERS: ' + JSON.stringify(res.headers));
-
 
       if (res.statusCode == 404) {
         console.log('err');
         return null;
       }
-
 
       if (res.statusCode == 200) {
         var data = "";
@@ -238,32 +192,19 @@ console.log('eeeeeeeeeend');
 
     }).on('error', function(e) {
       console.log('get error', e);
-    }); //http.get(get_options, function(res) {
-
-
-  } //,get: function(apikey, workspace, services, job, payload, jobid) {
-
-
-
-
-
-
-
+    }); 
+    
+  } 
+  
   ,batch: function(url, callback) {
 
     var httpb = require('https');
 
-//    var data;
     httpb.get(url, function(res) {
-      
-//      console.log("statusCode: ", res.statusCode);
-//      console.log("headers: ", res.headers);
-
       if (res.statusCode == 404) {
         console.log('err');
         return null;
       }
-
 
       if (res.statusCode == 200) {
         var data = "";
@@ -286,21 +227,11 @@ console.log('eeeeeeeeeend');
 
     }).on('error', function(e) {
       console.log('error batch', e);
-    }); //http.get(get_options, function(res) {
+    });
 
-
-
-
-  } //,get: function(apikey, workspace, services, job, payload, jobid) {
-
-
-
-
-
-
+  }
 
   ,delete: function(apikey, workspace, services, job, payload, jobid, callback) {
-
 
     var httpd = require('https');
 
@@ -311,32 +242,21 @@ console.log('eeeeeeeeeend');
       path: '/workspaces/' + workspace + '/services/' + services + '/' + job + '/' + jobid,
       method: 'DELETE',
       headers: {
-        //'Content-Type':'application/json;charset=utf-8',          
-        //'Accept': 'application/json',
         'Content-Length': 0,
         'Authorization': 'Bearer ' + apikey
       }
     };
 
-
     httpd.get(get_options, function(res) {
-
-
-//      console.log('STATUS: ' + res.statusCode);
-//      console.log('HEADERS: ' + JSON.stringify(res.headers));
-
 
       if (res.statusCode == 404) {
         console.log('err');
         return null;
       }
 
-
       if (res.statusCode == 204) {
         callback('delete');
       }
-
-
 
       if (res.statusCode == 200) {
         var data = "";
@@ -359,21 +279,13 @@ console.log('eeeeeeeeeend');
 
     }).on('error', function(e) {
       console.log('error delete:', e, jobid);
-    }); //http.get(get_options, function(res) {
+    }); 
 
+  } 
 
-  } //,get: function(apikey, workspace, services, job, payload, jobid) {
-
-
-
-
-
-    //https://management.azureml.net/workspaces/0360466b83b04998ab86ea69dbcec5bf/webservices/6ae5a8a1db174941b01f1fdcebe69377/endpoints/run
-    ,trained: function(apikey, workspace, services, endpoint, payload, callback) {
+   ,trained: function(apikey, workspace, services, endpoint, payload, callback) {
 
     var https_trained = require('https'); 
-    
-    //stringify to get length
     this.payload = JSON.stringify(payload); 
 
     if(debug) console.log(payload); 
@@ -446,21 +358,6 @@ console.log('eeeeeeeeeend');
       post_req.end();
     }
 
-  } //trained
-
-
-
-}; //module.exports = {
-
-
-
-
-
-
-
-
-
-
-
-
-
+  } 
+  
+}; 
